a073fd0ce144b0868ca3287091d6998301cc2b0c
Mark Norman Francis <norm@cackhanded.net>
2009-09-21 10:24:56 +0000

    Add first classifiers.

---
+++ Twitter/Filter/Classifier.pm
@@ -0,0 +1,214 @@
+package Twitter::Filter::Classifier;
+
+use Modern::Perl;
+use MooseX::FollowPBP;
+use Moose;
+use MooseX::Method::Signatures;
+
+with 'Twitter::Filter::State';
+with 'Twitter::Filter::Config';
+
+use constant PLUGIN_CLASS        => 'Twitter::Filter::Classifier';
+use constant QUEUE_TIMEOUT       => 0;      # 0 means "wait indefinitely"
+use constant QUEUE_POLL_INTERVAL => 15;
+use constant BAYES_STATE_FILE    => 'state/bayes.db';
+use constant WORD_IS_HASHTAG     => qr{^ [#] ( .+ ) $}x;
+use constant WORD_IS_MENTION     => qr{
+        ^
+        [^a-zA-Z0-9_]*          # start with things other than account chars
+        [@] ( [a-zA-Z0-9_]+ )   # followed by a normal @user construct
+        $
+    }ix;
+
+use Algorithm::NaiveBayes;
+use Algorithm::NaiveBayes::Model::Frequency;
+use File::stat;
+use IPC::DirQueue;
+use Module::Pluggable   require     => 1,
+                        search_path => [ PLUGIN_CLASS ];
+use Storable            qw( freeze thaw );
+
+has state_timestamp => ( isa => 'Int', is => 'rw' );
+has classifiers     => (
+        isa     => 'ArrayRef',
+        is      => 'ro',
+        builder => 'build_classifiers',
+    );
+has in_queue        => (
+        isa     => 'IPC::DirQueue', 
+        is      => 'ro',
+        builder => 'build_in_queue',
+    );
+has out_queue       => (
+        isa     => 'IPC::DirQueue', 
+        is      => 'ro',
+        builder => 'build_out_queue',
+    );
+has current_job     => (
+        isa => 'IPC::DirQueue::Job',
+        is  => 'rw',
+    );
+has bayes_state     => (
+        isa => 'Algorithm::NaiveBayes::Model::Frequency',
+        is  => 'rw',
+    );
+
+method build_in_queue {
+    return IPC::DirQueue->new( { dir => 'queues/collected' } );
+}
+method build_out_queue {
+    return IPC::DirQueue->new( { dir => 'queues/classified' } );
+}
+method build_classifiers {
+    my ( @plugins, @classifiers );
+    
+    # find plugins in @INC
+    foreach my $plugin ( $self->plugins() ) {
+        push @plugins, $plugin;
+    }
+    
+    # find plugins in the 'classifiers' directory
+    foreach my $file ( glob 'classifiers/*.pm' ) {
+        require $file;
+        
+        $file =~ s{^ classifiers/ (.*) \.pm $}{$1}x;
+        my $plugin = sprintf 
+                         "%s::%s",
+                             PLUGIN_CLASS,
+                             $file;
+        push @plugins, $plugin;
+    }
+    
+    foreach my $plugin ( @plugins ) {
+        if ( $plugin->can( 'classify' ) ) {
+            push @classifiers, $plugin;
+            
+            if ( $plugin->can( 'initialise' ) ) {
+                $plugin->initialise();
+            }
+        }
+        else {
+            say STDERR "** $plugin: cannot classify";
+        }
+    }
+    
+    return \@classifiers;
+}
+
+
+method classify_tweet ( HashRef $tweet ) {
+    my %tokens      = $self->tweet_token_list( $tweet );
+    my $final_score = 0;
+    my %final_tags;
+    my @buckets;
+    
+    # run tweet through each classifier plugin, collecting tags and score
+    foreach my $classifier ( @{ $self->get_classifiers } ) {
+        my ( $score, @tags ) 
+            = $classifier->classify( $self, $tweet, \%tokens );
+        
+        $final_score += $score
+                     // 0;
+        
+        foreach my $tag ( @tags ) {
+            $final_tags{ $tag } = 1;
+        }
+    }
+
+    my @tags = keys %final_tags;
+    
+    # run tweet through bayesian classification, putting it into buckets
+    # where the likelihood of belonging is greater than 3/4s
+    my $buckets = $self->get_tweet_buckets( $tweet, \%tokens );
+    foreach my $bucket ( keys %{ $buckets } ) {
+        if ( 0.75 < $buckets->{ $bucket } ) {
+            push @buckets, $bucket;
+        }
+    }
+    
+    return $final_score, \@tags, \@buckets;
+}
+method tweet_token_list ( HashRef $tweet ) {
+    my $text = $tweet->{'text'};
+    my $user = $tweet->{'user'}{'screen_name'};
+    
+    # who the tweet is from could be an important factor
+    my %tokens = (
+            'attr:user' => $user,
+        );
+    
+    foreach my $word ( split( /\s+/, $text ) ) {
+        $tokens{ $word }++;
+        
+        # weight the words with the person who said them
+        my $user_word = "%${user}%${word}";
+        $tokens{ $user_word }++;
+        
+        # add metadata about special words
+        if ( $word =~ WORD_IS_MENTION ) {
+            $tokens{'attr:mention'}++;
+            $tokens{"mention%$1"}++;
+        }
+        if ( $word =~ WORD_IS_HASHTAG ) {
+            $tokens{'attr:hashtag'}++;
+            $tokens{"hashtag%$1"}++;
+        }
+    }
+    
+    return %tokens;
+}
+method get_tweet_buckets ( HashRef $tweet, HashRef $tokens ) {
+    $self->reload_bayes_state();
+    my $bayes = $self->get_bayes_state();
+    
+    # beware: bayes object will not exist when nothing has been trained
+    if ( defined $bayes ) {
+        return $bayes->predict( attributes => $tokens );
+    }
+    
+    return;
+}
+method reload_bayes_state {
+    my $stat = stat( BAYES_STATE_FILE );
+    if ( $stat ) {
+        if ( $stat->mtime > $self->get_state_timestamp() ) {
+            $self->set_bayes_state( 
+                    Algorithm::NaiveBayes->restore_state( BAYES_STATE_FILE )
+                );
+            $self->set_state_timestamp( $stat->mtime );
+        }
+    }
+}
+
+
+method get_next_tweet {
+    my $queue    = $self->get_in_queue();
+    
+    my $job      = $queue->wait_for_queued_job( 
+                           QUEUE_TIMEOUT, QUEUE_POLL_INTERVAL 
+                       );
+    my $contents = $job->get_data();
+    
+    $self->set_current_job( $job );
+    
+    return thaw( $contents );
+}
+method cancel_processing {
+    my $job = $self->get_current_job();
+    
+    $job->return_to_queue();
+}
+method finished_processing {
+    my $job = $self->get_current_job();
+    
+    $job->finish();
+}
+method queue_tweet ( HashRef $tweet ) {
+    my $string = freeze $tweet;
+    my $queue  = $self->get_out_queue();
+    
+    $queue->enqueue_string( $string );
+}
+
+
+1;
---
+++ classifiers/ResolveLinks.pm
@@ -0,0 +1,49 @@
+package Twitter::Filter::Classifier::ResolveLinks;
+
+use Modern::Perl;
+
+use Data::Dumper;
+use LWP::UserAgent;
+use URI::Find;
+
+my $ua = LWP::UserAgent->new( max_redirect => 5 );
+
+
+
+sub classify {
+    my $self       = shift;
+    my $classifier = shift;
+    my $tweet      = shift;
+    my $tokens     = shift;
+    
+    my @tags;
+    my $finder = URI::Find->new(
+            sub {
+                my $object = shift;
+                my $text   = shift;
+                
+                # figure out where the link points finally, then store the
+                # hostname as a tag (e.g. for filtering against "youtube.com")
+                my $head = $ua->head( $text );
+                if ( $head->is_success() ) {
+                    my $uri  = $head->request->uri;
+                    my $host = $uri->host();
+                    
+                    # canonicalise www.youtube.com to youtube.com
+                    $host =~ s{^www\.}{};
+                    push @tags, "link:$host";
+                    
+                    # uncomment to replace short links in
+                    # tweet with the resolved version
+                    # return $uri;
+                }
+                return $text;
+            }
+        );
+        
+    $finder->find( \$tweet->{'text'} );
+    
+    return ( 0, @tags );
+}
+
+1;
